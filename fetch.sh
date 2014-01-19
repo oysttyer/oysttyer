1611974904c8362a8a991e2cc743bd439f0742b7
atomicules <base@atomicules.co.uk>
2014-01-19 23:09:26 +0000

    Add parsing function to deal with odd version numbering on Floodgap.
    E.g:
    
    - "02" should be "0.2"
    - "2.1.02" should be "2.1.2"

--- fetch.sh
+++ fetch.sh
@@ -20,15 +20,28 @@ get_lines_from_changelog() {
 	sed -n "$start,$end p" $changelog > commit.tmp
 }
 
-#What if version isn't found?
-#i.e. 2.0.00 
-#Also need to delete leading zero!!! 
+# Hacky cludge to fix tags from Floodgap:
+# 0.x versions are missing the decimals/periods
+# 1.x or 2.x versions have leading zeroes on the last digit.
+fixtag() {
+	if [ `expr "$1" : '0'` -ne 0 ]; then
+		if [ `expr length $1` -eq 2 ]; then
+			fixedtag=`echo $1 | sed 's/\(0\)\([0-9]\)/\1\.\2/'`
+		else
+			fixedtag=`echo $1 | sed 's/\(0\)\([0-9]\)\([0-9]*\)/\1\.\2\.\3/'`
+		fi
+	elif [ `expr "$1" : '[1-2]'` -ne 0 ]; then
+		fixedtag=`echo $1 | sed 's/\([0-9]\.[0-9]\.\)0\([0-9]\)/\1\2/'`
+	fi
+	echo $fixedtag
+}
 
 curl $DIST_URL | grep bytes | sed 's/"/ /g' | awk '{print $4, $8, $9, $10, $11, $12}' > dist.txt
 while read line; do
 	URL=`echo $line | awk '{print dist_url $1}' dist_url=$DIST_URL`
 	DATE=`echo $line | awk '{print $2",", $4, $3, $6, $5}'`
 	TAG=`echo $line | awk '{print $1}' | sed 's/\.txt//g'`
+	TAG=`fixtag $TAG`
 	get_lines_from_changelog CHANGELOG.markdown $TAG
 	curl $URL > ttytter.pl
 	git add ttytter.pl
